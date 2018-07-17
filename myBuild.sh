17e74fa729ea8af5511ba11418e4dd7ad49cdf99
lasssad <belhedilassad45@gmail.com>
2018-07-17 16:44:04 +0100

    Make mybuild executable

--- myBuild.sh
+++ myBuild.sh
@@ -1,3 +1,3 @@
 echo "You built your project using this script!"
-cp  -r /home/lassad/Documents/Documentation_nuxeo.odt /home/lassad/Images
+scp   /home/lassad/Documents/Documentation_nuxeo.odt /home/lassad/Images
 echo "hello world"
